5b535d7d77b660a91edc401d9e8f70159dc4ad0f
clara <poncetcla@gmail.com>
2023-12-13 17:53:13 +0100

    roman numerals

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,11 +1,73 @@
 package fr.istic.vv;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeraUtils {
-    
-        public static boolean isValidRomanNumeral(String value) { return false; }
-    
-        public static int parseRomanNumeral(String numeral) { return 0; }
-    
-        public static String toRomanNumeral(int number) { return ""; }
-    
+    public static final Map<String, Integer> numerals = new HashMap<String, Integer>() {{
+        put("I", 1);
+        put("IV", 4);
+        put("V", 5);
+        put("IX", 9);
+        put("X", 10);
+        put("L", 50);
+        put("XC", 90);
+        put("C", 100);
+        put("D", 500);
+        put("M", 1000);
+    }};
+
+    public static boolean isValidRomanNumeral(String value) {
+        return value.matches("[IVXLCDM]*") &&
+                !value.matches(".*I{4,}.*") &&
+                !value.matches(".*X{4,}.*") &&
+                !value.matches(".*C{4,}.*") &&
+                !value.matches(".*M{4,}.*") &&
+                !value.matches(".*V{2,}.*") &&
+                !value.matches(".*L{2,}.*") &&
+                !value.matches(".*D{2,}.*") &&
+                !value.matches(".*I{2,}V.*") &&
+                !value.matches(".*I{2,}X.*") &&
+                !value.matches(".*X{2,}C.*");
+    }
+
+    public static int parseRomanNumeral(String numeral) {
+        if (numeral.length() == 0) {
+            return 0;
+        }
+
+        if (numeral.length() == 1) {
+            return numerals.get(numeral);
+        }
+
+        char left, right;
+        int value = 0;
+        for (int i = 0; i < numeral.length(); i++) {
+            left = numeral.charAt(i);
+            right = i + 1 < numeral.length() ? numeral.charAt(i + 1) : '\0';
+            value += numerals.containsKey(left + "" + right) ? numerals.get(left + "" + right) : numerals.get(left + "");
+        }
+
+        return value;
+    }
+
+    public static String toRomanNumeral(int number) {
+
+        String res = "";
+
+        if (number == 0) {
+            return res;
+        }
+
+
+        for (Map.Entry<String, Integer> entry : numerals.entrySet()) {
+            while (number >= entry.getValue()) {
+                res += entry.getKey();
+                number -= entry.getValue();
+                break;
+            }
+        }
+
+        return res;
+    }
 }
--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,71 @@
 package fr.istic.vv;
+
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.Positive;
 
 public class RomanNumeralTest {
+
+    private boolean isRepeatedThriceAtMost(String aRomanNumber, char aCharacter) {
+        return !aRomanNumber.matches(".*" + aCharacter + aCharacter + aCharacter + aCharacter + ".*");
+    }
+
+    private boolean isNotRepeated(String aRomanNumber, char aCharacter) {
+        return !aRomanNumber.matches(".*" + aCharacter + aCharacter + ".*");
+    }
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
+    boolean absoluteValueOfAllNumbersIsPositive(@ForAll @Positive int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+    @Property
+    boolean romanNumberOnlyContainsValidCharacters(@ForAll int anInteger) {
+        String aRomanNumber = RomanNumeraUtils.toRomanNumeral(anInteger);
+
+        return aRomanNumber.matches("[IVXLCDM]*");
+    }
+
+    @Property
+    boolean romanNumberRepetitionRule1(@ForAll int anInteger) {
+        String aRomanNumber = RomanNumeraUtils.toRomanNumeral(anInteger);
+
+        return isRepeatedThriceAtMost(aRomanNumber, 'I') &&
+                isRepeatedThriceAtMost(aRomanNumber, 'X') &&
+                isRepeatedThriceAtMost(aRomanNumber, 'C') &&
+                isRepeatedThriceAtMost(aRomanNumber, 'M');
+
+    }
+
+    @Property
+    boolean romanNumberRepetitionRule2(@ForAll int anInteger) {
+        String aRomanNumber = RomanNumeraUtils.toRomanNumeral(anInteger);
+
+        return isNotRepeated(aRomanNumber, 'V') &&
+                isNotRepeated(aRomanNumber, 'L') &&
+                isNotRepeated(aRomanNumber, 'D');
+    }
+
+    @Property
+    boolean hasOnlyOneSubstractor(@ForAll int anInteger) {
+        String aRomanNumber = RomanNumeraUtils.toRomanNumeral(anInteger);
+
+        if (aRomanNumber.length() < 2) {
+            return true;
+        }
+
+        boolean res = true;
+        char left, right;
+        int leftValue, rightValue;
+        for (int i = 0; i < aRomanNumber.length(); i++) {
+            left = aRomanNumber.charAt(i);
+            right = i + 1 < aRomanNumber.length() ? aRomanNumber.charAt(i + 1) : '\0';
+            leftValue = RomanNumeraUtils.numerals.get(left + "");
+            rightValue = RomanNumeraUtils.numerals.get(right + "");
+            if (leftValue < rightValue) {
+                res = Character.toString(right).matches("[CXV]") && (leftValue == rightValue / 10 || leftValue == rightValue / 5);
+            }
+        }
+
+        return res;
     }
 }
