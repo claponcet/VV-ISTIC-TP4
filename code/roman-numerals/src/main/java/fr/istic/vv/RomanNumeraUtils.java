044f4f9f44c6eb72649ccd18dc015df6ee00baa4
elecoant <etienne.lecoant@etudiant.univ-rennes1.fr>
2023-12-13 21:03:35 +0100

    roman numerals

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,70 +1,115 @@
 package fr.istic.vv;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RomanNumeraUtils {
-    public static final Map<String, Integer> numerals = new HashMap<String, Integer>() {{
-        put("I", 1);
-        put("IV", 4);
-        put("V", 5);
-        put("IX", 9);
-        put("X", 10);
-        put("L", 50);
-        put("XC", 90);
-        put("C", 100);
-        put("D", 500);
-        put("M", 1000);
-    }};
+    public static final Map<String, Integer> numerals = new LinkedHashMap<String, Integer>() {
+        {
+            put("M", 1000);
+            put("CM", 900);
+            put("D", 500);
+            put("CD", 400);
+            put("C", 100);
+            put("XC", 90);
+            put("L", 50);
+            put("XL", 40);
+            put("X", 10);
+            put("IX", 9);
+            put("V", 5);
+            put("IV", 4);
+            put("I", 1);
+        }
+    };
 
-    public static boolean isValidRomanNumeral(String value) {
-        return value.matches("[IVXLCDM]*") &&
-                !value.matches(".*I{4,}.*") &&
-                !value.matches(".*X{4,}.*") &&
-                !value.matches(".*C{4,}.*") &&
-                !value.matches(".*M{4,}.*") &&
-                !value.matches(".*V{2,}.*") &&
-                !value.matches(".*L{2,}.*") &&
-                !value.matches(".*D{2,}.*") &&
-                !value.matches(".*I{2,}V.*") &&
-                !value.matches(".*I{2,}X.*") &&
-                !value.matches(".*X{2,}C.*");
+    public static boolean isValidRomanNumeral(String numeral) {
+        if (!numeral.matches("[MDCLXVI]*")) {
+            return false;
+        }
+
+        String[] repetitions = {
+                "VV", "LL", "DD", "IIII", "XXXX", "CCCC", "MMMM"
+        };
+
+        // I, X, C, M cannot be repeated more than 3 times.
+        // V, L, D cannot be repeated.
+        for (String repetition : repetitions) {
+            if (numeral.matches(repetition)) {
+                return false;
+            }
+        }
+
+        // Check if numeral contains valid pairs (ie IV, IX, XC, XL, CD, CM)
+
+        if (numeral.length() > 1) {
+            String currentNumeral = "", nextNumeral = "", pair = "";
+            int currentNumeralValue = 0, nextNumeralValue = 0;
+
+            int i = 0;
+            while (i < numeral.length()) {
+                currentNumeral = numeral.substring(i, i + 1);
+
+                if (i < numeral.length() - 1) {
+                    nextNumeral = numeral.substring(i + 1, i + 2);
+                }
+
+                currentNumeralValue = numerals.get(currentNumeral);
+                nextNumeralValue = numerals.get(nextNumeral);
+                pair = currentNumeral + nextNumeral;
+
+                if (nextNumeralValue > currentNumeralValue && !numerals.containsKey(pair)) {
+                    return false;
+                }
+
+                i++;
+            }
+        }
+
+        return true;
     }
 
     public static int parseRomanNumeral(String numeral) {
-        if (numeral.length() == 0) {
+        if (numeral == null || numeral.isEmpty() || !isValidRomanNumeral(numeral)) {
             return 0;
         }
 
-        if (numeral.length() == 1) {
-            return numerals.get(numeral);
+        int res = 0;
+
+        String currentNumeral = "", nextNumeral = "", pair = "";
+
+        int i = 0;
+        while (i < numeral.length()) {
+            currentNumeral = numeral.substring(i, i + 1);
+
+            if (i < numeral.length() - 1) {
+                nextNumeral = numeral.substring(i + 1, i + 2);
+            }
+
+            pair = currentNumeral + nextNumeral;
+            if (numerals.containsKey(pair)) {
+                res += numerals.get(pair);
+                i += 2;
+            } else {
+                res += numerals.get(currentNumeral);
+                i += 1;
+            }
         }
 
-        char left, right;
-        int value = 0;
-        for (int i = 0; i < numeral.length(); i++) {
-            left = numeral.charAt(i);
-            right = i + 1 < numeral.length() ? numeral.charAt(i + 1) : '\0';
-            value += numerals.containsKey(left + "" + right) ? numerals.get(left + "" + right) : numerals.get(left + "");
-        }
-
-        return value;
+        return res;
     }
 
     public static String toRomanNumeral(int number) {
 
         String res = "";
 
-        if (number == 0) {
+        if (number <= 0 || number > 3999) {
             return res;
         }
-
 
         for (Map.Entry<String, Integer> entry : numerals.entrySet()) {
             while (number >= entry.getValue()) {
                 res += entry.getKey();
                 number -= entry.getValue();
-                break;
             }
         }
 
